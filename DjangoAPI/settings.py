8eeb17d6ba3ffbbd1a9c1914f3b50504217bb4c9
touchavin <touchavin@hotmail.com>
2021-08-27 08:15:11 +0700

    first commit

--- DjangoAPI/settings.py
+++ DjangoAPI/settings.py
@@ -18,8 +18,7 @@ import django_heroku
 # BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,"media")
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -166,6 +165,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 # Activate Django-Heroku.
 django_heroku.settings(locals())
 
